37cb9bbec6e6740665c6b7438998fc0b9c4eb752
TheWalkingNev <alexandre.gasior@gmail.com>
2018-12-30 18:46:56 +0100

    mercredi c'est fini !

---
+++ precursus_website/main.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+  if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')){
+    document.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+  }
+}, 10000);
